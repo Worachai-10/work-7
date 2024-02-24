13c04cbafccfe68b556990a31e1806ef1ea01f5a
Worachai-10 <139629440+Worachai-10@users.noreply.github.com>
2024-02-24 00:46:27 -0800

    Create def3.py

---
+++ def3.py
@@ -0,0 +1,8 @@
+myuser = "admin"
+mypass = "Ad13n@23t"
+useruser = input("Username: ")
+passpass = input("Password: ")
+if useruser == myuser and passpass == mypass:
+    print("สำเร็จ")
+else:
+    print("เข้าสู่ระบบไม่สำเร็จ")
